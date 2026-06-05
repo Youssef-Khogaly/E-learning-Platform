525297d817dc6f8d758b7cdb1f7865eecc441a69
Youssef-Khogaly <yossefibrahem8@gmail.com>
2026-06-05 18:46:14 +0300

    dockerization

---
+++ DB-init/schema.sql
@@ -0,0 +1,133 @@
+
+
+create database if not exists E_learning;
+use E_learning;
+
+SET default_storage_engine = InnoDB;
+
+
+create table users(
+                      id bigint primary key auto_increment,
+                      name varchar(256) not null ,
+                      email varchar(256) unique not null ,
+                      password varchar(512) not null ,
+                      role enum('ADMIN','Instructor','Student') not null default 'Student'
+);
+
+
+create table courses(
+                        id bigint primary key auto_increment,
+                        title varchar(128) not null ,
+                        `desc` TEXT,
+                        priceInCents bigint not null ,
+                        currency varchar(6) not null ,
+                        state enum('DRAFT','PUBLISHED','UNPUBLISHED') not null ,
+                        publishedAt TIMESTAMP,
+                        unPublishedAt TIMESTAMP,
+                        instructor_id bigint not null ,
+                        constraint courseInstructor foreign key (instructor_id) references users(id)
+                            on update CASCADE  on delete restrict
+);
+
+
+create table if not exists sections(
+                         id bigint primary key auto_increment,
+                         `index` integer not null ,
+                         title varchar(128) not null,
+                         courseId bigint not null ,
+                         constraint courseSection foreign key (courseId) references courses(id)
+                             on update CASCADE  on delete restrict
+);
+
+
+create table if not exists lessons(
+                        id bigint primary key auto_increment,
+                        `index` integer,
+                        title varchar(128) not null,
+                        lastPublishedAt TIMESTAMP,
+                        lastUnpublishedAt TIMESTAMP,
+                        isPreview boolean default false,
+                        type enum('TXT' , 'VIDEO') not null,
+                        state enum('DRAFT','PUBLISHED','UNPUBLISHED') not null ,
+                        sectionId bigint not null ,
+                        constraint lessonSection foreign key (sectionId) references sections(id)
+                            on update CASCADE  on delete restrict
+);
+
+
+create table if not exists videos(
+                       id varchar(32) primary key not null ,
+                       title varchar(128) not null,
+                       isPublic boolean,
+                       isMp4Support boolean,
+                       createAt timestamp,
+                       updateAt timestamp,
+                       metaData json,
+                       status ENUM('uploading', 'UPLOADED', 'PROCESSING', 'READY') not null ,
+                       fileSize integer ,
+                       isPlayable boolean,
+                       width integer,
+                       height INT,
+                       bitrate BIGINT,
+                       duration integer,
+                       framerate integer,
+                       samplerate INT,
+                       video_codec VARCHAR(50),
+                       audio_codec VARCHAR(50),
+                       aspect_ratio VARCHAR(20),
+                       ownerId bigint not null,
+                       constraint videoOwner foreign key (ownerId) references users(id)
+                           on update cascade on delete restrict
+);
+
+
+create table if not exists video_encoded_qualities(
+                                        videoId varchar(32) not null ,
+                                        quality enum('_240p','_360p','_480p','_720p','_1080p','-2160p') not null ,
+                                        primary key (videoId,quality),
+                                        constraint video foreign key(videoId) references videos(id)
+);
+
+
+create table  if not exists lessonsContent(
+                               id bigint primary key not null ,
+                               txt TEXT default null,
+                               duration int ,
+                               videoId varchar(32),
+                               constraint videoLesson foreign key (videoId) references videos(id)
+                                   on update cascade on delete cascade ,
+
+                               constraint lesson foreign key (id)references lessons(id)
+);
+
+
+create table if not exists enrollments(
+                            id bigint primary key auto_increment,
+                            usrId bigint not null ,
+                            courseId bigint not null ,
+                            enrollDate timestamp not null ,
+                            constraint usrEnroll foreign key (usrId) references users(id)
+                                on update cascade on delete restrict,
+                            constraint courseEnroll foreign key (courseId) references courses(id)
+                                on update CASCADE  on delete restrict,
+                            constraint uniqueEnroll unique (usrId,courseId)
+);
+
+
+create table if not exists payments(
+                         id BINARY(16) primary key ,
+                         userId bigint not null ,
+                         priceInCents bigint not null ,
+                         currency varchar(6) not null ,
+                         method enum('STRIPE') not null,
+                         status enum('EXPIRED','FAILED','PENDING','SUCCESS') not null,
+                         course_id bigint not null ,
+                         transaction_id varchar(255),
+                         session_id varchar(255) unique not null ,
+                         paidAt bigint null ,
+                         createdAt TIMESTAMP,
+                         constraint usrPayment foreign key (userId) references users(id)
+                             on update cascade on delete restrict,
+                         constraint coursePayment foreign key (course_id) references courses(id)
+                             on update CASCADE  on delete restrict
+);
